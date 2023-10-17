887d231fca205e6a30c7b49426196b10bea77e1b
sujon sarder <SUJONSARDER92@GMAIL.COM>
2023-10-17 11:16:23 +0200

    user delete button is available

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,27 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const users = useLoaderData();
-  console.log(users);
+
+  const [loggedUsers, setLoggedUsers] = useState(users);
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const remaining = users.filter((user) => user._id !== id);
+        setLoggedUsers(remaining);
+      });
+  };
+
   return (
     <div>
-      <h2 className="capitalize text-4xl underline font-semibold ">Users data table </h2>
+      <h2 className="capitalize text-4xl underline font-semibold ">
+        Users data table{" "}
+      </h2>
 
       <div className="overflow-x-auto">
         <table className="table table-zebra">
@@ -19,12 +35,19 @@ const Users = () => {
             </tr>
           </thead>
           <tbody className="mx-auto">
-            {users.map((user) => (
+            {loggedUsers.map((user) => (
               <tr className="bg-purple-400" key={user._id}>
                 <td>{user._id}</td>
                 <td>{user.email}</td>
                 <td>{user.createdAt}</td>
-                <td><Link className="btn btn-neutral">x</Link></td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-neutral"
+                  >
+                    x
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
